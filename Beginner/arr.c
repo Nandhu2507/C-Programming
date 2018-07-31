8df5f2f5d6b6de7e6d70a471d8f512e39e01cfa1
Nandhu2507 <38652905+Nandhu2507@users.noreply.github.com>
2018-07-31 11:05:57 +0000

    Update arr.c

--- Beginner/arr.c
+++ Beginner/arr.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 int main()
 {
-	int i,num,arr[20]={20,34,37,24,67,56,45,89,45,1,3,7};
-	printf("enter a number:");
+	int max=0,i,num,arr[20]={20,34,37,24,67,56,45,89,45,1,3,7};
 	scanf("%d",&num);
+	
 	for(i=0;i<num;i++)
 	{
 		printf("%d\t",arr[i]);
-		
+		if(arr[i]>max)
+		{
+			max=arr[i];
+		}
 	}
+	printf("\n%d",max);
 	return 0;
 	
 }
